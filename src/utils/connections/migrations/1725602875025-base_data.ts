08bad13e1d6e6e819f97475e188fb951a7d70f69
jecs27 <jesus.calderon2708@gmail.com>
2024-09-05 23:10:37 -0700

    chore: prettier config applied

--- src/utils/connections/migrations/1725602875025-base_data.ts
+++ src/utils/connections/migrations/1725602875025-base_data.ts
@@ -1,16 +1,21 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class BaseData1725602875025 implements MigrationInterface {
-    name = 'BaseData1725602875025'
+  name = 'BaseData1725602875025';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users" ("userId" BIGSERIAL NOT NULL, "name" character varying NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, "email" character varying NOT NULL, CONSTRAINT "UQ_450a05c0c4de5b75ac8d34835b9" UNIQUE ("password"), CONSTRAINT "PK_97672ac88f789774dd47f7c8be3" PRIMARY KEY ("email"))`);
-        await queryRunner.query(`CREATE INDEX "IDX_8bf09ba754322ab9c22a215c91" ON "users" ("userId") `);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'CREATE TABLE "users" ("userId" BIGSERIAL NOT NULL, "name" character varying NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, "email" character varying NOT NULL, CONSTRAINT "UQ_450a05c0c4de5b75ac8d34835b9" UNIQUE ("password"), CONSTRAINT "PK_97672ac88f789774dd47f7c8be3" PRIMARY KEY ("email"))',
+    );
+    await queryRunner.query(
+      'CREATE INDEX "IDX_8bf09ba754322ab9c22a215c91" ON "users" ("userId") ',
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."IDX_8bf09ba754322ab9c22a215c91"`);
-        await queryRunner.query(`DROP TABLE "users"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'DROP INDEX "public"."IDX_8bf09ba754322ab9c22a215c91"',
+    );
+    await queryRunner.query('DROP TABLE "users"');
+  }
 }
